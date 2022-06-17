6f093d3dfcb11b98613d7aeb3746237d6b5e6421
Joe Park <sunghun.josephp@gmail.com>
2022-06-16 23:52:33 -0700

    Creat DB connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // Dependencies
 require('dotenv').config();
+require('./config/config');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // App listener
 app.listen(PORT, (res, req) => {
